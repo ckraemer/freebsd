7f9b94d998cabc8147f8d2a39241f0b113a87ec3
Christian Krämer <uddka@student.kit.edu>
2018-05-16 16:13:08 +0200

    Implemented gpiointr_attach() and gpiointr_interrupt_handler().

--- sys/dev/gpio/gpiointr.c
+++ sys/dev/gpio/gpiointr.c
@@ -2,6 +2,7 @@
 #include <sys/kernel.h>
 #include <sys/bus.h>
 #include <sys/module.h>
+#include <sys/gpio.h>
 
 #include <dev/fdt/fdt_common.h>
 #include <dev/ofw/ofw_bus.h>
@@ -9,11 +10,17 @@
 #include <dev/gpio/gpiobusvar.h>
 
 struct gpiointr_softc {
+	device_t	dev;
+	gpio_pin_t	pin;
+	int		intr_rid;
+	struct resource	*intr_res;
+	void		*intr_cookie;
 };
 
-static int gpiointr_probe(device_t);
-static int gpiointr_attach(device_t);
-static int gpiointr_detach(device_t);
+static int	gpiointr_probe(device_t);
+static int	gpiointr_attach(device_t);
+static int	gpiointr_detach(device_t);
+static void	gpiointr_interrupt_handler(void*);
 
 static int
 gpiointr_probe(device_t dev) {
@@ -28,7 +35,55 @@ gpiointr_probe(device_t dev) {
 
 static int
 gpiointr_attach(device_t dev) {
+	struct gpiointr_softc *sc;
+	phandle_t node;
+	int err;
+
 	device_printf(dev, "attach\n");
+
+	sc = device_get_softc(dev);
+	sc->dev = dev;
+
+	node = ofw_bus_get_node(dev);
+	if(node == -1)
+	{
+		device_printf(dev, "no node in fdt\n");
+		return (ENXIO);
+	}
+
+	if(!OF_hasprop(node, "gpiopin"))
+	{
+		device_printf(dev, "no gpiopin property in fdt\n");
+		return (ENXIO);
+	}
+
+	err = gpio_pin_get_by_ofw_propidx(dev, node, "gpiopin", 0, &sc->pin);
+	if(err != 0)
+	{
+		device_printf(dev, "no valid gpiopin in fdt");
+		return (err);
+	}
+
+	device_printf(dev, "gpiopin is pin number %d\n", sc->pin->pin);
+
+	sc->intr_res = gpio_alloc_intr_resource(dev, &sc->intr_rid, RF_ACTIVE, sc->pin, GPIO_INTR_EDGE_FALLING);
+	if(sc->intr_res == NULL)
+	{
+		device_printf(dev, "cannot allocate interrupt resource\n");
+		return(ENXIO);
+	}
+
+	device_printf(dev, "interrupt resource allocated\n");
+
+	err = bus_setup_intr(dev, sc->intr_res, INTR_TYPE_MISC | INTR_MPSAFE, NULL, gpiointr_interrupt_handler, sc, &sc->intr_cookie);
+	if(err != 0)
+	{
+		device_printf(dev, "cannot set up interrupt\n");
+		return (err);
+	}
+
+	device_printf(dev, "interrupt resource set up\n");
+
 	return (0);
 }
 
@@ -36,6 +91,14 @@ static int
 gpiointr_detach(device_t dev) {
 	device_printf(dev, "detach\n");
 	return (0);
+}
+
+static void
+gpiointr_interrupt_handler(void *arg)
+{
+	struct gpiointr_softc *sc = arg;
+
+	device_printf(sc->dev, "interrupt handler executed!\n");
 }
 
 static device_method_t gpiointr_methods[] = {
